8caa82ef42382579aa7300b151f85790f50e493d
Badsha <abdullahbinbadsha01@gmail.com>
2026-06-26 16:21:04 +0600

    added javascript for contact us buton message

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("contactBtn");
+
+button.addEventListener("click", function () {
+    alert("Hello sir,\nWelcome to our website!");
+    console.log("Java Connected!"); //this is for console
+});
